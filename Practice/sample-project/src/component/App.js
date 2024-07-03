e0c8a7dffc930d6d37cbbb0be63a03aa36d01089
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2024-07-03 22:27:31 +0530

    Create App.js

---
+++ Practice/sample-project/src/component/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Title from "./Title.js";
+
+function App(props) {
+
+  
+  return (
+    <div>
+      <Title value="Joi" />
+      
+    </div>
+  );
+}
+
+export default App;
